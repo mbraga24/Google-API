838d69a673077e58298c9373dae6f6a4bfe8b156
Marlon Braga <rdg.brg@gmail.com>
2019-07-27 16:50:26 -0400

    10-AddingNRemovingControls

---
+++ Google Maps Practice/Practice Lessons/10-AddingNRemovingControls/main.js
@@ -0,0 +1,25 @@
+let map;
+
+function initMap() {
+  let mapOptions = {
+    zoom: 10,
+    center: {lat: 40.758904, lng:-73.985128},
+    zoomControl: true,
+    zoomControlOptions: {
+      position: google.maps.ControlPosition.TOP_RIGHT
+    },
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.TOP_RIGHT,
+      // style: google.maps.MapTypeControlStyle.DROPDOWN_MENU    
+    },
+    scaleControl: false,
+    streetViewControl: false,
+    rotateControl: false
+  }
+
+  map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions)
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize)
